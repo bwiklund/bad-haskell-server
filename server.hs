6ca970f73a04bcf6372f3d47fa6b13aba40a9769
Ben Wiklund <ben@daisyowl.com>
2014-12-23 12:53:42 -0800

    server test

---
+++ server.hs
@@ -0,0 +1,24 @@
+import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
+import System.Environment (getArgs)
+import System.IO (hSetBuffering, hGetLine, hPutStrLn, hClose, BufferMode(..), Handle)
+import Control.Concurrent (forkIO)
+
+main :: IO ()
+main = withSocketsDo $ do
+  args <- getArgs
+  let port = fromIntegral (read $ head args :: Int)
+  sock <- listenOn $ PortNumber port
+  sockHandler sock
+
+sockHandler :: Socket -> IO ()
+sockHandler sock = do
+  (handle, _, _) <- accept sock
+  hSetBuffering handle NoBuffering
+  forkIO $ commandProcessor handle
+  sockHandler sock
+
+commandProcessor :: Handle -> IO ()
+commandProcessor handle = do
+  line <- hGetLine handle
+  hPutStrLn handle "200 OK\n\nwelp."
+  hClose handle
