a5aa066eb15a6dd735d59d2ac2815dd5ef891815
Ben Wiklund <ben@daisyowl.com>
2014-12-24 13:45:41 -0800

    type annotations, handler type

--- server.hs
+++ server.hs
@@ -3,15 +3,19 @@ module Server where
 import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
 import Control.Concurrent (forkIO)
 import System.IO (hSetBuffering, hGetLine, hPutStrLn, hPutStr, hIsEOF, hClose, BufferMode(..), Handle)
-import qualified Request
+import Request
+
+type Handler = (IO Request -> IO String)
 
 -- starts a server
+listen :: Int -> Handler -> IO ()
 listen port handler = withSocketsDo $ do
   sock <- listenOn $ PortNumber $ fromIntegral port
   putStrLn "listening"
   sockHandler sock handler
 
 -- recursively accepts connections and forks a handler
+sockHandler :: Socket -> Handler -> IO ()
 sockHandler sock handler = do
   (handle, _, _) <- accept sock
   hSetBuffering handle NoBuffering
@@ -19,8 +23,9 @@ sockHandler sock handler = do
   sockHandler sock handler
 
 -- entry point for a request. parses headers and passes the request to the user's handler
+requestHandler :: Handle -> Handler -> IO ()
 requestHandler handle handler = do
-  let request = Request.fromHandle handle
+  let request = fromHandle handle
   response <- handler request
   hPutStr handle response
   hClose handle
