b5e2d4bdb1779b24581b41909c86b684f14e5e98
Ben Wiklund <ben@daisyowl.com>
2014-12-26 18:56:38 -0800

    stubbing out a little router

--- usage.hs
+++ usage.hs
@@ -1,12 +1,26 @@
 import Server
+import Request
 
 import System.Environment (getArgs)
 
 main = do
   args <- getArgs
   let port = read $ head args :: Int
-  listen port echoHandler
+  listen port router
 
+matchers = [("/echo", echoHandler)]
+
+router :: Handler
+router request = do
+  (snd $ head matchers) request
+
+echoHandler :: Handler
 echoHandler request = do
   response <- request
   return ("You sent: \n\n" ++ (show response) ++ "\n\nCordially,\n  Haskell")
+
+type Matcher = (String, Handler)
+
+routeRequest :: [Matcher] -> Request -> IO String
+routeRequest router request = do
+  return ("hello this is router")
