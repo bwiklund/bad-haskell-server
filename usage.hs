b347a0b174fabc6003f744be67847271a2bb4e96
Ben Wiklund <ben@daisyowl.com>
2014-12-26 21:52:43 -0800

    ow my brain

--- usage.hs
+++ usage.hs
@@ -5,21 +5,21 @@ import System.Environment (getArgs)
 import Text.Regex.Posix
 import Data.List (find)
 
-main = do
-  args <- getArgs
-  let port = read $ head args :: Int
-  listen port router
-
 type Matcher = (String, Handler)
+data Router = Router {matchers :: [Matcher]}
 
-matchers = [("/echo", echoHandler)]
+fooRouter = Router [("/echo", echoHandler)]
 
-router :: Handler
-router request = do
-  let matchedHandler = find (\(re,_) -> (uri request) =~ re :: Bool) matchers
+routeRequest router = \request -> do
+  let matchedHandler = find (\(re,_) -> (uri request) =~ re :: Bool) (matchers router)
   case matchedHandler of
     Just handler -> (snd handler) request
     Nothing -> return ("HTTP/1.1 404 Non Found\nContent-Type: text/plain\n\n404 Can't Even")
+
+main = do
+  args <- getArgs
+  let port = read $ head args :: Int
+  listen port $ routeRequest fooRouter
 
 echoHandler :: Handler
 echoHandler request = do
