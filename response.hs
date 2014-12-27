459a4b0b6cb3471f31443792a66202994c8b00e5
Ben Wiklund <ben@daisyowl.com>
2014-12-27 08:52:15 -0800

    cleanup

--- response.hs
+++ response.hs
@@ -2,6 +2,12 @@ module Response where
 
 import qualified Data.Map as Map
 
+data Response = Response {
+  status :: Int,
+  headers :: Map.Map String String,
+  body :: String
+} deriving (Show)
+
 statusCodes =
   Map.fromList [
     (200, "OK"),
@@ -14,16 +20,12 @@ lookupStatusMessage code =
     Nothing -> "Shrug"
     Just message -> message
 
-data Response = Response {
-  status :: Int,
-  headers :: Map.Map String String,
-  body :: String
-} deriving (Show)
-
 headersToString headers =
   let headerToString (k,v) = k ++ ": " ++ v
    in unlines $ map headerToString $ Map.toList headers
 
+-- i could just make an instance of Show, but that would make it hard to debug by logging.
+-- or maybe not. i'm on a plane and don't have wifi.
 toString :: Response -> String
 toString response =
   unlines [
--- usage.hs
+++ usage.hs
@@ -7,12 +7,13 @@ import qualified Data.Map
 
 fooRouter = Router [("/echo", echoHandler)]
 
+echoHandler :: Handler
+echoHandler request = do
+  let response = request
+      body = "You sent: \n\n" ++ (show response) ++ "\n\nCordially,\n  Haskell"
+  return (Response 200 Data.Map.empty body)
+
 main = do
   args <- getArgs
   let port = read $ head args :: Int
   listen port $ routeRequest fooRouter
-
-echoHandler :: Handler
-echoHandler request = do
-  let response = request
-  return (Response 200 Data.Map.empty ("You sent: \n\n" ++ (show response) ++ "\n\nCordially,\n  Haskell"))
